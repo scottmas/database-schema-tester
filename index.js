60e36a9b07c0ddcc55cecc8f237ff76623595ad9
Scott Ashton <sashton@instructure.com>
2018-01-23 07:21:36 -0700

    Support integer ids

--- index.js
+++ index.js
@@ -17,63 +17,63 @@ const sequelize = new Sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
 });
 
 const csv = `
-team			
+team			 
 id	name		
-team_real	Real SLC		
+1	Real SLC		
 			
 account			
 id	name		
-tyson	Tyson the coach		
-miles	Miles the older kid		
-henry	Henry the smaller kid		
-kevin	Kevin the father of Henry		
-ana	Ana the helper		
+1	Tyson the coach		
+2	Miles the older kid		
+3	Henry the smaller kid		
+4	Kevin the father of Henry		
+5	Ana the helper		
 			
 user_role			
 id	granted_to	in_behalf_of	role_id
-tyson	miles	miles	userEdit
-miles	henry	henry	userView
-henry	kevin	henry	userVIew
+1	2	2	4
+2	3	3	5
+3	4	3	5
 			
 role			
-id			
-teamCoach			
-teamHelper			
-teamPlayer			
-userEdit			
-userView			
-			
-team_role			
-user_id	role_id		
-tyson	teamCoach		
-tyson	teamPlayer		
-miles	teamPlayer		
-henry	teamPlayer		
-ana	teamHelper		
+id	name		
+1	teamCoach		
+2	teamHelper		
+3	teamPlayer		
+4	userEdit		
+5	userView		
 			
 permission			
-id			
-TEAM_UPDATE_ROSTER			
-TEAM_ADMIN			
-TEAM_VIEW_GAMES			
-USER_UPDATE_NAME
-USER_UPDATE_AVATAR			
-PROXY_FULL_			
+id	name		
+1	TEAM_UPDATE_ROSTER		
+2	TEAM_ADMIN		
+3	TEAM_VIEW_GAMES		
+4	USER_UPDATE_NAME		
+5	USER_UPDATE_AVATAR		
+6	PROXY_FULL_		
 			
 role_permission			
-role_id	permission_id		
-teamCoach	TEAM_UPDATE_ROSTER		
-teamCoach	TEAM_ADMIN		
-teamCoach	TEAM_VIEW_GAMES		
-teamPlayer	TEAM_UPDATE_ROSTER		
-teamPlayer	TEAM_VIEW_GAMES		
+id	role_id	permission_id	
+1	1	1	
+2	1	2	
+3	1	3	
+4	2	1	
+5	2	3	
 			
 team_roster			
-team_id	user_id		
-team_real	tyson		
-team_real	miles		
-team_real	henry		
-team_real	ana		
+id	team_id	user_id	
+1	1	1	
+2	1	2	
+3	1	3	
+4	1	5	
+			
+team_role			
+id	user_id	role_id	
+1	1	1	
+2	1	3	
+3	2	3	
+4	3	3	
+5	5	2	
 `;
 
 main().catch(console.error);
@@ -84,9 +84,11 @@ async function main(){
     const data = str.split('\n').slice(3).map(r => r.split(/\t+/).filter(v => v)).filter(v => v.length);
 
     await sequelize.query(`DROP TABLE IF EXISTS ${table};`);
-    await sequelize.query(`CREATE TABLE ${table} (\n${columns.map(c => '  ' + c + ' varchar(40)').join(',\n')}\n);`);
+    await sequelize.query(`CREATE TABLE ${table} (
+      ${columns.map(c => c + (c.includes('id') ? ' bigint' : ' varchar(40)')).join(',\n')}
+    );`);
     for(let row of data){
-      await sequelize.query(`INSERT INTO ${table} (${columns.join(', ')}) VALUES (${row.map(v => `'${v}'`).join(', ')});`)
+      await sequelize.query(`INSERT INTO ${table} (${columns.join(', ')}) VALUES (${row.map(v => isNumeric(v) ? v : `'${v}'`).join(', ')});`)
     }
   }
 
@@ -95,4 +97,8 @@ async function main(){
   console.log("Selecting all the users...", results);
 
   await sequelize.close();
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
